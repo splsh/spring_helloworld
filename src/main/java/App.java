656fa5d9580c1189e495c5bccdf33b969f46bf57
Alexei <splash.dll@gmail.com>
2023-04-18 13:35:07 +0300

    Spring hello

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld = applicationContext.getBean(HelloWorld.class);
+        Cat cat = applicationContext.getBean(Cat.class);
+        Cat tiger =  applicationContext.getBean(Cat.class);
+        System.out.println(bean==helloWorld);
+        System.out.println(cat==tiger);
+        ((AnnotationConfigApplicationContext) applicationContext).close();
     }
 }
